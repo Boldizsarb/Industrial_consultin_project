bbf0ba0bc8e11aa853ec41efc7806d59e3465876
Guildb <5silvr53@solent.ac.uk>
2024-02-21 14:55:22 +0000

    added the front end to the git project still need to make it work properly

---
+++ frontend/src/views/Signup.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <div
+      class="w-2/3 flex min-h-screen max-w-screen-sm items-center justify-center transition-opacity duration-700 ease-in opacity-0 fade-bottom"
+      :class="{ 'opacity-100': isAnimated }"
+      @mouseenter="isAnimated = true"
+    >
+      <div class="relative bg-gray-200 bg-opacity-50 rounded-lg p-4 w-full">
+        <router-link to="/" class="absolute top-0 right-0 m-4">
+          <img
+            src="@/assets/images/back.png"
+            alt="Back to Home"
+            class="h-8 w-8"
+          />
+          <!-- Adjust size with h-8 w-8 -->
+        </router-link>
+        <div>
+          <h2
+            class="mt-8 text-center text-4xl font-extrabold text-slate-700 tracking-tight"
+          >
+            Create Account
+          </h2>
+        </div>
+        <form class="mt-8 space-y-6" @submit.prevent="login">
+          <input type="hidden" name="remember" value="true" />
+          <div class="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label for="email-address" class="sr-only">Email address</label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autocomplete="email"
+                required
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label for="password" class="sr-only">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                required
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div
+            class="flex flex-col sm:flex-row items-center justify-center space-y-2 sm:space-y-0 sm:space-x-2"
+          >
+            <button
+              type="submit"
+              class="w-full sm:w-auto bg-slate-700 hover:bg-green-700 flex-1 justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Signup
+            </button>
+            <router-link
+              to="/login"
+              class="w-full sm:w-auto bg-slate-700 hover:bg-green-700 inline-flex items-center justify-center flex-1 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Login
+            </router-link>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginPage",
+  data() {
+    return {
+      credentials: {
+        username: "",
+        password: "",
+      },
+      isAnimated: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isAnimated = true;
+    }, 100); // Start the animation shortly after the component mounts
+  },
+  methods: {
+    async login() {
+      try {
+        //const response = await axios.post("/api/login", this.credentials);
+        // Save the token, usually in local storage, and redirect
+        //localStorage.setItem("user-token", response.data.token); // Token saved
+        this.$router.push({ name: "Dashboard" }); // Redirect to home
+      } catch (error) {
+        console.error("Login error:", error);
+        // Handle error, show message to the user
+      }
+    },
+  },
+};
+</script>
