2538d9d555765a6ddda07a9cff3bd3362b72afd8
Oladapo Giwa-Daramola <91552016+Oladapo01@users.noreply.github.com>
2024-02-29 19:15:26 +0000

    Updated the Signup.vue to fix the Signup error

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -47,6 +47,17 @@
                 class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Last Name"
               />
+              <label for="email" class="sr-only">Email</label>
+              <input
+                id="email"
+                name="email"
+                type="text"
+                v-model="email"
+                autocomplete="email"
+                required
+                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Last Name"
+              />
             </div>
             <div>
               <label for="phoneNumber" class="sr-only"
@@ -76,6 +87,7 @@
                 placeholder="Password"
               />
             </div>
+
             <div>
               <label for="confirmpassword" class="sr-only"
                 >Confirm Password</label
@@ -140,7 +152,7 @@ export default {
         return;
       }
       console.log(process.env.VUE_APP_BACKEND_URL);
-      fetch(`${process.env.VUE_APP_BACKEND_URL}/signup`, {
+      fetch(`http://localhost:5000/signup`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
