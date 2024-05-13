8199fa6ef6b0128756393ce30d8ff662aefd8f73
fmilheir <contada42@gmail.com>
2024-05-13 16:02:10 +0100

    testing hasura

--- hasura/hasura-entrypoint.sh
+++ hasura/hasura-entrypoint.sh
@@ -2,31 +2,31 @@
 
 # Wait for the database service to be ready
 echo "Waiting for the database to be ready..."
-while ! nc -z industrial-consulting-postgresql 5432; do   
-  sleep 0.1
-done
+if ! nc -z industrial-consulting-postgresql 5432; then
+    echo "Database is not ready. Waiting..."
+    sleep 5
+fi
 echo "Database is ready."
 
-# Wait for Hasura to be ready
-echo "Waiting for Hasura to be ready..."
-#while ! nc -z industrial-consulting-hasura 8080; do   
-#  echo "Checking 1"
-#  sleep 0.1
-#done
+echo "Checking if Hasura is ready..."
+if ! nc -z industrial-consulting-hasura 8080; then
+    echo "Hasura is not ready. Waiting..."
+    sleep 5
+fi
 echo "Hasura is ready."
 
-echo "Checking if Hasura is fully ready..."
-#until $(curl --output /dev/null --silent --head --fail http://industrial-consulting-hasura:8080/healthz); do
-#   printf '.'
-#   echo "Checking 2"
-#   sleep 5
-#done
-#echo "Hasura is fully operational."
-
+echo "Checking if Hasura is fully operational..."
+if ! curl --output /dev/null --silent --head --fail http://industrial-consulting-hasura:8080/healthz; then
+    echo "Hasura is not fully operational. Waiting..."
+    sleep 5
+fi
+echo "Hasura is fully operational."
 
 # Apply metadata
 echo "Applying Hasura metadata..."
-hasura metadata apply --endpoint http://industrial-consulting-hasura:8080 --admin-secret postegres --log-level debug
+mkdir -p /hasura-metadata
+cp /hasura_metadata.json /hasura-metadata/
+hasura metadata apply --endpoint http://industrial-consulting-hasura:8080 --admin-secret postegres --metadata-dir /hasura-metadata --log-level debug
 
 # Start the Hasura GraphQL engine
 echo "Starting Hasura GraphQL engine..."
